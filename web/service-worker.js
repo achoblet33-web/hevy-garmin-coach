66fc3d466174c70b6825bf41c4cc396faeaa1a06
achoblet33-web <a.choblet33@gmail.com>
2026-09-04 09:37:36 -0400

    Refresh TrainSync PWA cache

--- web/service-worker.js
+++ web/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "trainsync-v1";
-const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
+const CACHE_NAME = "trainsync-v2";
+const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./hotfix.js", "./manifest.webmanifest", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
